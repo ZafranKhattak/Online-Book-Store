9e3a4a5207751773413d1cd098f363ba647f0926
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-07 01:42:55 +0500

    first commit

--- BooksPage/Book.java
+++ BooksPage/Book.java
@@ -12,20 +12,78 @@ public class Book {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
-        // ===================== METHO CASLL CREATE IMAGE =====================//
+        // ===================== JScrollPanel ===================== //
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setPreferredSize(new Dimension(1000, 1500));
 
-        JLabel b1 = createImage("BooksPage/img1.png", 0, 10, 300, 500);
-        JLabel b2 = createImage("BooksPage/img2.jpeg",310, 10,300, 500 );
-        JLabel b3 = createImage("BooksPage/img3.png",630, 10,300, 500 );
-        JLabel b4 = createImage("BooksPage/img2.jpeg",940, 10,300, 500 );
-        // JLabel b5 = createImage("BooksPage/img2.jpeg",340, 10,300, 500 );
-        
+        JScrollPane scroll = new JScrollPane(panel);
+
+        scroll.setBounds(0, 0, 1300, 900);
+        scroll.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.setHorizontalScrollBarPolicy(
+            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        frame.add(scroll);
+
+        // ===================== METHOD CALL CREATE IMAGE =====================//
+
+        String images[][] = {
+                {
+                        "BooksPage/img1.png",
+                        "BooksPage/img2.jpeg",
+                },
+                {
+                        "BooksPage/img3.png",
+                        "BooksPage/img4.jpeg",
+                },
+                {
+                        "BooksPage/img5.jpeg",
+                        "BooksPage/img6.jpeg",
+                },
+                {
+                        "BooksPage/img7.jpeg",
+                        "BooksPage/img8.jpeg",
+                },
+                {
+                        "BooksPage/img9.jpeg",
+                        "BooksPage/img10.jpeg",
+                },
+                {
+                        "BooksPage/img11.jpeg",
+                        "BooksPage/img12.jpeg",
+                },
+                {
+                        "BooksPage/img13.jpeg",
+                        "BooksPage/img14.jpeg",
+                }
+        };
+
+        for (int i = 0; i < images.length; i++) {
+            for (int j = 0; j <images[i].length; j++) {
+                JLabel book = createImage(
+                        images[i][j],
+                        i * 250,
+                        j * 350,
+                        220,
+                        300);
+                panel.add(book);
+            }
+        }
+        // JLabel b1 = createImage("BooksPage/img1.png", 0, 10, 300, 400);
+        // JLabel b2 = createImage("BooksPage/img2.jpeg",320, 10,300, 400 );
+        // JLabel b3 = createImage("BooksPage/img3.png",640, 10,300, 400 );
+        // JLabel b4 = createImage("BooksPage/img4.jpeg",960, 10,300, 400 );
+        // JLabel b5 = createImage("BooksPage/img5.jpeg",0, 430,300, 400 );
+
         // ===================== ADD TO FRAME ==========================
 
-        frame.add(b1);
-        frame.add(b2);
-        frame.add(b3);
-        frame.add(b4);
+        // panel.add(b1);
+        // panel.add(b2);
+        // panel.add(b3);
+        // panel.add(b4);
+        // panel.add(b5);
+        frame.add(scroll);
     }
 
     public static JLabel createImage(String path, int x, int y, int width, int height) {
